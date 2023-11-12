71c23a9bceb61747578c3246db9372fc7559548c
ycis5013@naver.com <ycis5013@naver.com>
2023-11-13 01:27:23 +0900

    code : PlanDto에 대표 이미지 img 추가

--- backend/src/main/java/com/ssafy/plan/model/PlanDto.java
+++ backend/src/main/java/com/ssafy/plan/model/PlanDto.java
@@ -24,14 +24,17 @@ public class PlanDto {
 	private String regDate;
 	@ApiModelProperty(value = "여행계획 설명")
 	private String planDetail;
+	@ApiModelProperty(value = "여행계획 대표 이미지")
+	private String img;
 	@ApiModelProperty(value = "여행명소 리스트")
 	private List<AttractionInfoDto> attrInfoList;
 	
 	public PlanDto() {
 		super();
 	}
+
 	public PlanDto(int planId, String userId, String planName, String startDate, String endDate, String regDate,
-			String planDetail) {
+			String planDetail, String img, List<AttractionInfoDto> attrInfoList) {
 		super();
 		this.planId = planId;
 		this.userId = userId;
@@ -40,73 +43,87 @@ public class PlanDto {
 		this.endDate = endDate;
 		this.regDate = regDate;
 		this.planDetail = planDetail;
-	}
-	
-	
-	public PlanDto(int planId, String userId, String planName, String startDate, String endDate, String regDate,
-			String planDetail, List<AttractionInfoDto> attrInfoList) {
-		super();
-		this.planId = planId;
-		this.userId = userId;
-		this.planName = planName;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.regDate = regDate;
-		this.planDetail = planDetail;
+		this.img = img;
 		this.attrInfoList = attrInfoList;
 	}
+
 	public int getPlanId() {
 		return planId;
 	}
+
 	public void setPlanId(int planId) {
 		this.planId = planId;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getPlanName() {
 		return planName;
 	}
+
 	public void setPlanName(String planName) {
 		this.planName = planName;
 	}
+
 	public String getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
+
 	public String getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
+
 	public String getRegDate() {
 		return regDate;
 	}
+
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
+
 	public String getPlanDetail() {
 		return planDetail;
 	}
+
 	public void setPlanDetail(String planDetail) {
 		this.planDetail = planDetail;
 	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
 	public List<AttractionInfoDto> getAttrInfoList() {
 		return attrInfoList;
 	}
+
 	public void setAttrInfoList(List<AttractionInfoDto> attrInfoList) {
 		this.attrInfoList = attrInfoList;
 	}
+
 	@Override
 	public String toString() {
 		return "PlanDto [planId=" + planId + ", userId=" + userId + ", planName=" + planName + ", startDate="
-				+ startDate + ", endDate=" + endDate + ", regDate=" + regDate + ", planDetail=" + planDetail
-				+ ", attrInfoList=" + attrInfoList + "]";
+				+ startDate + ", endDate=" + endDate + ", regDate=" + regDate + ", planDetail=" + planDetail + ", img="
+				+ img + ", attrInfoList=" + attrInfoList + "]";
 	}
+
 }
