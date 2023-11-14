6808a7073049e596e2bd9844371473a070319891
youngjoooh <dhdudwn95@hansung.ac.kr>
2023-11-15 04:23:05 +0900

    code: 여행계획추가시 장소검색 및 추가 기능 확장

--- frontend/src/views/PlanAddView.vue
+++ frontend/src/views/PlanAddView.vue
@@ -6,6 +6,8 @@ import api from "axios";
 
 const router = useRouter();
 const route = useRoute();
+const searchWord = ref("");
+const searchResult = ref([]);
 const plan = ref({
   userId: "JohnOh",
   planName: "",
@@ -16,8 +18,18 @@ const plan = ref({
   img: "https://img.freepik.com/free-photo/airplane_74190-464.jpg?w=1380&t=st=1699807779~exp=1699808379~hmac=aa5cc0c5c8e05a2a1437b84eec67fc7e174e450c93e37d6996ca134b2a9a4184",
 });
 
+const addPlace = (place)=>{
+  plan.value.attrInfo.push(place);
+  searchResult.value = [];
+  console.log(plan.value.attrInfo);
+}
+
 const hasAttr = computed(() => {
   return plan.value.attrInfo.length > 0 ? true : false;
+})
+
+const hasSearchResult = computed(()=>{
+  return searchResult.value.length >0?true:false;
 })
 
 const addPlan = async () => {
@@ -32,6 +44,20 @@ const addPlan = async () => {
     })
     .then(() => {
       router.push({ path: "/planlist" });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const searchAttraction = async () => {
+  await api
+    .post(`http://localhost:8090/trip/attraction/search`,
+      searchWord.value, { headers: { 'Content-Type': 'application/text' } }
+    )
+    .then(({ data }) => {
+      searchResult.value = data;
+      console.log(searchResult.value);
     })
     .catch((e) => {
       console.log(e);
@@ -60,33 +86,33 @@ const addPlan = async () => {
 
                   <h6 class="card-subtitle mx-auto d-block mb-3">[{{ plan.userId }}]님</h6>
 
-                <input type="text" class="form-control mb-3" id="startDate" placeholder="시작날짜를 입력하세요."
-                  v-model="plan.startDate">
-                <input type="text" class="form-control" id="endDate" placeholder="마지막날짜를 입력하세요." v-model="plan.endDate">
+                  <input type="text" class="form-control mb-3" id="startDate" placeholder="시작날짜를 입력하세요."
+                    v-model="plan.startDate">
+                  <input type="text" class="form-control" id="endDate" placeholder="마지막날짜를 입력하세요." v-model="plan.endDate">
 
-                <h4 class="box-title mt-5">[ 세부 내용 ]</h4>
-                <textarea class="form-control mb-5" id="planDetail" rows="3" v-model="plan.planDetail"
-                  placeholder="세부 내용을 입력하세요."></textarea>
-              </div>
-            </div>
-
-
-
-            <!-- 여행장소추가부분 -->
-
-            <div v-show="hasAttr" class="timeline">
-              <div class="timeline-row" v-for="(attrInfo, index) in plan.attrInfoList" :key="index">
-                <div class="timeline-time">
-                  7:45PM<small>Dec 21</small>
+                  <h4 class="box-title mt-5">[ 세부 내용 ]</h4>
+                  <textarea class="form-control mb-5" id="planDetail" rows="3" v-model="plan.planDetail"
+                    placeholder="세부 내용을 입력하세요."></textarea>
                 </div>
-                <div class="timeline-content">
-                  <i class="icon-attachment"></i>
-                    <h4>{{ attrInfo.title }}</h4>
+              </div>
+
+
+
+              <!-- 여행장소추가부분 -->
+
+              <div v-show="hasAttr" class="timeline">
+                <div class="timeline-row" v-for="(attr, index) in plan.attrInfo" :key="index">
+                  <div class="timeline-time">
+                    7:45PM<small>Dec 21</small>
+                  </div>
+                  <div class="timeline-content">
+                    <i class="icon-attachment"></i>
+                    <h4>{{ attr.title }}</h4>
                     <p>여기서는 무엇을 할것입니다. 첫번째 여행지의 세부내용입니다.여기서는 무엇을 할것입니다. 첫번째 여행지의 세부내용입니다.여기서는 무엇을 할것입니다. 첫번째 여행지의
                       세부내용입니다.여기서는 무엇을 할것입니다. 첫번째 여행지의 세부내용입니다.여기서는 무엇을 할것입니다. 첫번째 여행지의 세부내용입니다.여기서는 무엇을 할것입니다. 첫번째 여행지의
                       세부내용입니다.</p>
                     <div class="thumbs">
-                      <img class="img-fluid rounded" :src="attrInfo.firstImage" alt="Maxwell Admin">
+                      <img class="img-fluid rounded" :src="attr.firstImage" alt="Maxwell Admin">
                     </div>
                   </div>
                 </div>
@@ -94,10 +120,32 @@ const addPlan = async () => {
 
 
               <div class="m-2 mb-3 row justify-content-center">
-                <div class="input-group justify-content-center col-md-2">
-                  <input type="text" class="form-control" placeholder="장소를 검색하세요."
-                    aria-label="장소를 검색하세요." aria-describedby="button-addon2">
-                  <button class="btn btn-outline-secondary" type="button" id="button-addon2">검색</button>
+                <div class="input-group justify-content-center mb-1">
+                  <input type="text" class="form-control" placeholder="장소를 검색하세요." aria-label="장소를 검색하세요."
+                    aria-describedby="button-addon2" v-model="searchWord">
+                  <button class="btn btn-outline-secondary" type="button" id="button-addon2"
+                    @click="searchAttraction()">검색</button>
+                </div>
+                
+                <div class="justify-content-center container-fluid">
+                  <!-- 검색결과리스트 -->
+                  
+                  <div v-show="hasSearchResult" class="overflow-y-scroll h-100 bg-body-tertiary p-3 rounded-2">
+                    <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%"
+                      data-bs-smooth-scroll="true" class="bg-body-tertiary rounded-2" tabindex="0">
+
+                      <ul class="list-group" v-for="(place, index) in searchResult" :key="index" >
+
+                        <a href="#" @click="addPlace(place)" class="list-group-item list-group-item-action" aria-current="true">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{{ place.title }}</h5>
+                          </div>
+                          <p class="mb-1">{{ place.addr1 }}</p>
+                        </a>
+                      </ul>
+                      
+                    </div>
+                  </div>
                 </div>
               </div>
 
