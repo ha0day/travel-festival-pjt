2542d72f30878026a1c50499b6868ea335073689
wjsrnr20 <wjsrnr20@gmail.com>
2023-11-15 15:03:53 +0900

    code: maven 프로젝트 업데이트

---
+++ backend/src/test/java/com/ssafy/EnjoyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
