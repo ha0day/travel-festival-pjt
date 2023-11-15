344dd0b4a9b707fc041be8be200b4ea71619214f
wjsrnr20 <wjsrnr20@gmail.com>
2023-11-15 15:52:51 +0900

    code: 로그인 뷰 생성

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,277 @@
-<script setup></script>
+<script setup>
+//npm install jwt-decode
+import { onMounted, reactive } from "vue";
+import { userStore } from "@/stores/userStore";
+import { useRouter } from "vue-router";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+const router = useRouter();
+const ustore = userStore();
+//npm i vue3-toastify
 
+const click = reactive({
+  rememeberId: false,
+});
+const user = reactive({
+  id: "",
+  password: "",
+});
+const error = reactive({
+  message: "",
+});
+function updateData(data, key) {
+  console.log(click);
+  user[key] = data;
+  console.log(user);
+}
+//쿠키에서 저장된 아이디가 있으면 가져오는 함수
+async function getIDByCookie() {
+  const cookies = document.cookie;
+  const cookieArray = cookies.split("; ");
+  console.log(cookieArray);
+  for (let i = 0; i < cookieArray.length; i++) {
+    const cookie = cookieArray[i].split("=");
+    const key = cookie[0];
+    const value = cookie[1];
+
+    if (key === "id") {
+      user.id = value;
+    }
+  }
+}
+
+//아이디를 쿠키에 저장하는 함수
+async function storeIDByCookie(id) {
+  const cookies = document.cookie;
+  const cookieArray = cookies.split("; ");
+  document.cookie = `id=${id}`;
+}
+
+async function signIn() {
+  console.log(user);
+  await ustore.userConfirm(user);
+  let token = sessionStorage.getItem("access-token");
+  console.log("1. confirm() token >> " + token);
+  if (ustore.isLogin) {
+    // 여기는 state commit 변경 사항이 잘 적용되는데..
+    await ustore.getUserInfo(token);
+    // await this.getUserInfo(token);
+    console.log("4. confirm() userInfo :: ", ustore.userInfo);
+    if (ustore.userInfo.isAdmin) {
+      toast.success(ustore.userInfo.id + "관리자 님 환영합니다!", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+      });
+    } else {
+      toast.success(ustore.userInfo.id + "님 환영합니다!", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+      });
+      // alert(store.state.userStore.userInfo.id + "님 환영합니다!");
+    }
+    if (click.rememeberId) {
+      // 아이디 저장 필요함
+      await storeIDByCookie(user.id);
+      await getIDByCookie();
+    }
+    router.push({ name: "about" }); // 메인 페이지로 이동
+  } else {
+    error.message = "아이디 또는 비밀번호가 잘못되었습니다.";
+  }
+}
+</script>
 <template>
-  <div></div>
+  <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+    <!--
+      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
+              id="bd-theme"
+              type="button"
+              aria-expanded="false"
+              data-bs-toggle="dropdown"
+              aria-label="Toggle theme (auto)">
+        <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
+        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
+      </button>
+      -->
+    <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
+      <li>
+        <button
+          type="button"
+          class="dropdown-item d-flex align-items-center"
+          data-bs-theme-value="light"
+          aria-pressed="false"
+        >
+          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
+            <use href="#sun-fill"></use>
+          </svg>
+          Light
+          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          class="dropdown-item d-flex align-items-center"
+          data-bs-theme-value="dark"
+          aria-pressed="false"
+        >
+          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
+            <use href="#moon-stars-fill"></use>
+          </svg>
+          Dark
+          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          class="dropdown-item d-flex align-items-center active"
+          data-bs-theme-value="auto"
+          aria-pressed="true"
+        >
+          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
+            <use href="#circle-half"></use>
+          </svg>
+          Auto
+          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+        </button>
+      </li>
+    </ul>
+  </div>
+
+  <form class="form-signin w-100 m-auto" @submit.prevent>
+    <h1 class="h3 mb-3 fw-normal">로그인</h1>
+
+    <div class="form-floating">
+      <input
+        type="text"
+        class="form-control"
+        id="floatingInput"
+        placeholder="Id"
+        v-model="user.id"
+      />
+      <label for="floatingInput">아이디</label>
+    </div>
+    <div class="form-floating">
+      <input
+        type="password"
+        class="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+        v-model="user.password"
+      />
+      <label for="floatingPassword">비밀번호</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value="remember-me"
+        id="flexCheckDefault"
+        @change="click.rememeberId = !click.rememeberId"
+      />
+      <label class="form-check-label" for="flexCheckDefault"> 아이디 저장 </label>
+    </div>
+    <button class="btn btn-primary w-100 py-2" type="submit" :click="signIn()">Sign in</button>
+  </form>
 </template>
 
-<style scoped></style>
+<style>
+.form-signin {
+  max-width: 700px;
+  padding: 1rem;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="text"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.b-example-divider {
+  width: 100%;
+  height: 3rem;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: solid rgba(0, 0, 0, 0.15);
+  border-width: 1px 0;
+  box-shadow: inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1), inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
+}
+
+.b-example-vr {
+  flex-shrink: 0;
+  width: 1.5rem;
+  height: 100vh;
+}
+
+.bi {
+  vertical-align: -0.125em;
+  fill: currentColor;
+}
+
+.nav-scroller {
+  position: relative;
+  z-index: 2;
+  height: 2.75rem;
+  overflow-y: hidden;
+}
+
+.nav-scroller .nav {
+  display: flex;
+  flex-wrap: nowrap;
+  padding-bottom: 1rem;
+  margin-top: -1px;
+  overflow-x: auto;
+  text-align: center;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+
+.btn-bd-primary {
+  --bd-violet-bg: #712cf9;
+  --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+  --bs-btn-font-weight: 600;
+  --bs-btn-color: var(--bs-white);
+  --bs-btn-bg: var(--bd-violet-bg);
+  --bs-btn-border-color: var(--bd-violet-bg);
+  --bs-btn-hover-color: var(--bs-white);
+  --bs-btn-hover-bg: #6528e0;
+  --bs-btn-hover-border-color: #6528e0;
+  --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+  --bs-btn-active-color: var(--bs-btn-hover-color);
+  --bs-btn-active-bg: #5a23c8;
+  --bs-btn-active-border-color: #5a23c8;
+}
+
+.bd-mode-toggle {
+  z-index: 1500;
+}
+
+.bd-mode-toggle .dropdown-menu .active .bi {
+  display: block !important;
+}
+</style>
