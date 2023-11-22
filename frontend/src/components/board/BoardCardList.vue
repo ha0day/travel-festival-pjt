9da93d2ddf2442ee1cca88f3a4b35e593c6e3338
wjsrnr20 <wjsrnr20@gmail.com>
2023-11-22 17:38:23 +0900

    code: 내가 좋아요한 것들만 보이도록 프론트 구현

--- frontend/src/components/board/BoardCardList.vue
+++ frontend/src/components/board/BoardCardList.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { ref, onMounted, defineProps } from "vue";
+import { ref, onMounted, watch } from "vue";
 import BoardCard from "./BoardCard.vue";
 import api from "axios";
 import { searchStore } from "@/stores/planListStore";
 import { userStore } from "@/stores/userStore";
-
 const sstore = searchStore();
 const ustore = userStore();
-
 const planList = ref([]);
+const onlyFavoriteList = ref([]);
+const onlyFavoriteToggle = ref("");
 const boardlist = async () => {
   await api
     .post(`${import.meta.env.VITE_VUE_API_URL}/plan`, {
@@ -24,11 +24,32 @@ const boardlist = async () => {
       } else {
         planList.value = data.filter((d) => d.shared === 1);
       }
+
+      onlyFavoriteList.value = planList.value.filter((plan) => {
+        console.log("하이", plan.isFavorite);
+        return plan.isFavorite === true;
+      });
+      console.log("onlyFavoriteList", onlyFavoriteList.value);
     })
     .catch((e) => {
       console.log(e);
     });
 };
+
+// const getFavoriteList = computed(() => {
+//   if(sstore.onlyFavorite
+
+//   return planList.filter((plan) => plan.isFavorite === 1);
+// });
+watch(
+  () => sstore.onlyFavorite,
+  () => {
+    if (sstore.onlyFavorite) {
+      onlyFavoriteToggle.value = true;
+    } else onlyFavoriteToggle.value = false;
+    console.log("간다간다");
+  }
+);
 
 onMounted(() => {
   boardlist();
@@ -37,9 +58,17 @@ onMounted(() => {
 
 <template>
   <div class="col-md-12">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div v-if="!onlyFavoriteToggle" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       <div v-for="(plan, index) in planList" :key="index">
         <board-card :plan="plan"></board-card>
+      </div>
+    </div>
+
+    <div class="col-md-12">
+      <div v-if="onlyFavoriteToggle" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div v-for="(plan, index) in onlyFavoriteList" :key="index">
+          <board-card :plan="plan"></board-card>
+        </div>
       </div>
     </div>
   </div>
